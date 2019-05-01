46b02a2f5ef168a603e805bce0ec9d613ec208a3
Fabian Schliski <kombustor@gmail.com>
2019-05-01 14:34:53 +0200

    Starting correct file

--- scripts/docker-dev.ps1
+++ scripts/docker-dev.ps1
@@ -18,7 +18,7 @@ function UpdateDeps {
 function RunApp {
     Write-Host -fore yellow "Running app..."
     Push-Location ($PSScriptRoot + "\..")
-    docker run --rm -w "/app" -v ${PWD}:/app lensor-dev python init.py
+    docker run --rm -w "/app" -v ${PWD}:/app lensor-dev python main.py
     Pop-Location
 }
 
