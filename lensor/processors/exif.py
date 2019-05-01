f8efcacdd9979ffd2dcbb32847e1ca1b7fdec4ae
Fabian Schliski <kombustor@gmail.com>
2019-05-01 17:22:54 +0200

    Added flake8 and fixed errors

--- lensor/processors/exif.py
+++ lensor/processors/exif.py
@@ -1,8 +1,10 @@
 import exifread
 
+
 def read_exif(file):
     tags = exifread.process_file(file, details=False)
 
-    # Read GPS data if available (GPS GPSLatitude, GPS GPSLongitude), original time captured (EXIF DateTimeOriginal)
+    # Read GPS data if available (GPS GPSLatitude, GPS GPSLongitude),
+    # original time captured (EXIF DateTimeOriginal)
 
     print(tags.keys())
