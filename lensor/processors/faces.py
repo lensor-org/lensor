e98544c215c66ac61b42f401987a43352ebb1f74
Scharom <scharom@pm.me>
2019-05-04 16:16:19 +0000

    Added basic face recognition

--- lensor/processors/faces.py
+++ lensor/processors/faces.py
@@ -1,21 +1,59 @@
+from PIL import Image
 import face_recognition
-from PIL import Image, ImageDraw
+import numpy as np
 
 
-def show_faces():
-    image = face_recognition.load_image_file(
-        "./lensor/processors/example_faces.jpg"
-    )
-    face_locations = face_recognition.face_locations(image)
+class FaceDetection:
+    IMAGE_DOWNSCALING_FACTOR = 0.25
+    FACE_MATCHING_THRESHOLD = 0.6
 
-    pil_image = Image.fromarray(image)
-    draw = ImageDraw.Draw(pil_image)
+    def __init__(self, user_id, image_id):
+        self.user_id = user_id
+        self.image_id = image_id
+        self.known_persons = dict(zip(
+            self._get_known_faces(), self._get_known_names()))
+        self.loaded_image = self._load_image()
 
-    for face_location in face_locations:
-        # Print the location of each face in this image
-        top, right, bottom, left = face_location
+    def _get_known_faces(self):
+        # TODO Db call
+        return
 
-        draw.rectangle(((left, top), (right, bottom)), outline=(255, 0, 0), )
+    def _get_known_names(self):
+        # TODO Db call
+        return
 
-    del draw
-    pil_image.save("./temp/out.jpg", "JPEG")
+    def _load_image(self):
+        # TODO Db call to retrive the image path by image id
+        image_path = './lensor/processors/example_faces.jpg'
+        image = Image.open(image_path)
+        width, height = image.size
+
+        # make image smaller for faster processing
+        image.resize(
+            (int(width * FaceDetection.IMAGE_DOWNSCALING_FACTOR),
+             int(height * FaceDetection.IMAGE_DOWNSCALING_FACTOR)))
+        image.convert('RGB')
+        return list(image.getdata())
+
+    def get_faces(self):
+        face_locations = face_recognition.face_locations(self.loaded_image)
+        detected_faces = face_recognition.face_encodings(
+            self.loaded_image, face_locations)    
+        unknown_faces = []
+        known_faces = []
+
+        for index, face in detected_faces:
+            face_distances = face_recognition.face_distance(
+                self.known_faces, face)
+            best_match = np.argmin(face_distances)
+            location = face_locations[index]
+
+            # is the face unknown?
+            if (best_match >= FaceDetection.FACE_MATCHING_THRESHOLD):
+                unknown_faces.append((face, location))
+            else:
+                known_faces.append(self.known_persons[face], face, location)
+
+        # TODO save_unknown_faces(self.user_id, self.image_id, unknown_faces)
+        # to db, best async
+        return zip(unknown_faces, known_faces)
