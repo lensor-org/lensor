45cdba8530171a63103db8dd1f1e2f1830a43338
Jerome Würf <scharom@pm.me>
2019-05-05 18:19:44 +0000

    Implemented db-connector

--- lensor/db/init_db.py
+++ lensor/db/init_db.py
@@ -1,10 +1,21 @@
-import os
-import sqlalchemy
+from os import environ
+
+from sqlalchemy import create_engine
 
 
 class InitDB:
 
-    def __init__():
-        db_env_vars = _read_db_env_vars()
+    def __init__(self):
+        self.db_user, self.db_passwd, self.db_name,
+        self.db_url = self._read_db_env_vars()
 
-    def _read_db_env_vars
+    def _read_db_env_vars(self):
+        return (environ['POSTGRES_USER'],
+                environ['POSTGRES_PASSWORD'],
+                environ['POSTGRES_DB'],
+                environ['POSTGRES_URL'])
+
+    def get_engine(self):
+        return create_engine('postgresql://' + self.db_user + ':' +
+                             self.db_passwd + '@' + self.db_url + '/' +
+                             self.db_name)
--- lensor/main.py
+++ lensor/main.py
@@ -1,7 +1,12 @@
+from lensor.db.init_db import InitDB
 from lensor.processing.imageinfo import ImageInfo
 
 
 def run():
+
+    db_engine = InitDB().get_engine()
+    db_engine.connect()
+    print(db_engine)
     image_file = open('./lensor/processing/examples/example.jpg', 'r+b')
 
     image_info = ImageInfo(image_id="temp_id", image=image_file)
